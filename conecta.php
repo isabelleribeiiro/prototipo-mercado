783d522f66845d117c5b46074d62ef0b0e25fd4b
Isabelle Ribeiro <ribeiroisabelle535@gmail.com>
2025-03-19 10:06:29 -0300

    conecta.php

--- conecta.php
+++ conecta.php
@@ -1,2 +1,2 @@
 <?php
-$conexao = mysqli_connect('sql100.infinityfree.com', 'if0_38157878','bpaCZ9P2oaIwT1','if0_38157878_loja');
+$conexao = mysqli_connect('sql100.infinityfree.com', 'if0_38157878','sua senha','if0_38157878_loja');
